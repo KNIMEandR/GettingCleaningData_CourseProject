bffbadae4ed2b8cc3bea4bd1f1d247d02e212e33
Lars Roeglin <github.2016@dontbugme.de>
2019-12-03 00:34:14 +0100

    Initial commit * Import files with metadata * Import training dataset and use metadata for cleaning

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+
+# Read activity labels and feature names to allow for properly labeling the datasets
+
+activity_labels <- read.delim("01 - RAW-DATA/activity_labels.txt",
+			      header=FALSE,
+			      sep=" ") %>%
+	rename(Label = V1,
+	       Activity.Name=V2)
+
+feature_names <- activity_labels <- read.delim("01 - RAW-DATA/features.txt",
+					       header=FALSE,
+					       sep=" ") %>%
+	rename(Activity.Label = V1,
+	       Feature=V2)
+
+# Read training dataset and merge into single dataframe
+
+subject_train <- read.delim("01 - RAW-DATA/train/subject_train.txt",
+			    header=FALSE) %>%
+	rename(Subject = V1)
+
+activities_train <- read.delim("01 - RAW-DATA/train/y_train.txt",
+			       header=FALSE) %>%
+	rename(Label = V1) %>%
+	inner_join(activity_labels)
+
+measurements_train <- read.delim("01 - RAW-DATA/train/X_train.txt",
+				 sep = "",
+				 header=FALSE,
+				 stringsAsFactors = FALSE,
+				 col.names = feature_names$Feature) 
+
+training_data <- cbind(subject_train, activities_train, measurements_train)
